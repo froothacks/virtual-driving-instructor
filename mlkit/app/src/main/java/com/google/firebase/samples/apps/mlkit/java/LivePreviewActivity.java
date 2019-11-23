6b4e9232d8cefd290496caa4ca616f3373fd3da9
Advait-M <advait.maybhate9@gmail.com>
2019-11-23 07:56:34 -0500

    Delete extra files and clean up structure.

--- mlkit/app/src/main/java/com/google/firebase/samples/apps/mlkit/java/LivePreviewActivity.java
+++ mlkit/app/src/main/java/com/google/firebase/samples/apps/mlkit/java/LivePreviewActivity.java
@@ -14,18 +14,14 @@
 package com.google.firebase.samples.apps.mlkit.java;
 
 import android.content.Context;
-import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.hardware.Camera;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemSelectedListener;
-import android.widget.ArrayAdapter;
 import android.widget.CompoundButton;
 import android.widget.Spinner;
 import android.widget.Toast;
@@ -38,22 +34,20 @@ import androidx.core.app.ActivityCompat.OnRequestPermissionsResultCallback;
 import androidx.core.content.ContextCompat;
 
 import com.google.android.gms.common.annotation.KeepName;
-import com.google.firebase.ml.vision.objects.FirebaseVisionObjectDetectorOptions;
 import com.google.firebase.samples.apps.mlkit.R;
 import com.google.firebase.samples.apps.mlkit.common.CameraSource;
 import com.google.firebase.samples.apps.mlkit.common.CameraSourcePreview;
 import com.google.firebase.samples.apps.mlkit.common.GraphicOverlay;
-import com.google.firebase.samples.apps.mlkit.java.automl.AutoMLImageLabelerProcessor;
-import com.google.firebase.samples.apps.mlkit.java.automl.AutoMLImageLabelerProcessor.Mode;
-import com.google.firebase.samples.apps.mlkit.java.barcodescanning.BarcodeScanningProcessor;
-import com.google.firebase.samples.apps.mlkit.java.custommodel.CustomImageClassifierProcessor;
+//import com.google.firebase.samples.apps.mlkit.java.automl.AutoMLImageLabelerProcessor;
+//import com.google.firebase.samples.apps.mlkit.java.automl.AutoMLImageLabelerProcessor.Mode;
+//import com.google.firebase.samples.apps.mlkit.java.barcodescanning.BarcodeScanningProcessor;
+//import com.google.firebase.samples.apps.mlkit.java.custommodel.CustomImageClassifierProcessor;
 import com.google.firebase.samples.apps.mlkit.java.facedetection.FaceContourDetectorProcessor;
-import com.google.firebase.samples.apps.mlkit.java.facedetection.FaceDetectionProcessor;
-import com.google.firebase.samples.apps.mlkit.java.imagelabeling.ImageLabelingProcessor;
-import com.google.firebase.samples.apps.mlkit.java.objectdetection.ObjectDetectorProcessor;
-import com.google.firebase.samples.apps.mlkit.common.preference.SettingsActivity;
-import com.google.firebase.samples.apps.mlkit.common.preference.SettingsActivity.LaunchSource;
-import com.google.firebase.samples.apps.mlkit.java.textrecognition.TextRecognitionProcessor;
+//import com.google.firebase.samples.apps.mlkit.java.imagelabeling.ImageLabelingProcessor;
+//import com.google.firebase.samples.apps.mlkit.java.objectdetection.ObjectDetectorProcessor;
+//import com.google.firebase.samples.apps.mlkit.common.preference.SettingsActivity;
+//import com.google.firebase.samples.apps.mlkit.common.preference.SettingsActivity.LaunchSource;
+//import com.google.firebase.samples.apps.mlkit.java.textrecognition.TextRecognitionProcessor;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -89,6 +83,10 @@ public final class LivePreviewActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Log.d(TAG, "onCreate");
+        if (!allPermissionsGranted()) {
+            getRuntimePermissions();
+        }
+
         setContentView(R.layout.activity_live_preview);
 
         preview = findViewById(R.id.firePreview);
@@ -101,25 +99,25 @@ public final class LivePreviewActivity extends AppCompatActivity
         }
 
         Spinner spinner = findViewById(R.id.spinner);
-        List<String> options = new ArrayList<>();
-        options.add(FACE_CONTOUR);
-        options.add(FACE_DETECTION);
-        options.add(AUTOML_IMAGE_LABELING);
-        options.add(OBJECT_DETECTION);
-        options.add(TEXT_DETECTION);
-        options.add(BARCODE_DETECTION);
-        options.add(IMAGE_LABEL_DETECTION);
-        options.add(CLASSIFICATION_QUANT);
-        options.add(CLASSIFICATION_FLOAT);
+//        List<String> options = new ArrayList<>();
+//        options.add(FACE_CONTOUR);
+//        options.add(FACE_DETECTION);
+//        options.add(AUTOML_IMAGE_LABELING);
+//        options.add(OBJECT_DETECTION);
+//        options.add(TEXT_DETECTION);
+//        options.add(BARCODE_DETECTION);
+//        options.add(IMAGE_LABEL_DETECTION);
+//        options.add(CLASSIFICATION_QUANT);
+//        options.add(CLASSIFICATION_FLOAT);
         // Creating adapter for spinner
-        ArrayAdapter<String> dataAdapter = new ArrayAdapter<>(this, R.layout.spinner_style,
-                options);
-        // Drop down layout style - list view with radio button
-        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        // attaching data adapter to spinner
-        spinner.setAdapter(dataAdapter);
-        spinner.setOnItemSelectedListener(this);
-
+//        ArrayAdapter<String> dataAdapter = new ArrayAdapter<>(this, R.layout.spinner_style,
+//                options);
+//        // Drop down layout style - list view with radio button
+//        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+//        // attaching data adapter to spinner
+//        spinner.setAdapter(dataAdapter);
+//        spinner.setOnItemSelectedListener(this);
+//
         ToggleButton facingSwitch = findViewById(R.id.facingSwitch);
         facingSwitch.setOnCheckedChangeListener(this);
         // Hide the toggle button if there is only 1 camera
@@ -134,19 +132,58 @@ public final class LivePreviewActivity extends AppCompatActivity
         }
     }
 
+    private String[] getRequiredPermissions() {
+        try {
+            PackageInfo info =
+                    this.getPackageManager()
+                            .getPackageInfo(this.getPackageName(), PackageManager.GET_PERMISSIONS);
+            String[] ps = info.requestedPermissions;
+            if (ps != null && ps.length > 0) {
+                return ps;
+            } else {
+                return new String[0];
+            }
+        } catch (Exception e) {
+            return new String[0];
+        }
+    }
+
+    private boolean allPermissionsGranted() {
+        for (String permission : getRequiredPermissions()) {
+            if (!isPermissionGranted(this, permission)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private void getRuntimePermissions() {
+        List<String> allNeededPermissions = new ArrayList<>();
+        for (String permission : getRequiredPermissions()) {
+            if (!isPermissionGranted(this, permission)) {
+                allNeededPermissions.add(permission);
+            }
+        }
+
+        if (!allNeededPermissions.isEmpty()) {
+            ActivityCompat.requestPermissions(
+                    this, allNeededPermissions.toArray(new String[0]), PERMISSION_REQUESTS);
+        }
+    }
+
     @Override
     public synchronized void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
-        // An item was selected. You can retrieve the selected item using
-        // parent.getItemAtPosition(pos)
-        selectedModel = parent.getItemAtPosition(pos).toString();
-        Log.d(TAG, "Selected model: " + selectedModel);
-        preview.stop();
-        if (allPermissionsGranted()) {
-            createCameraSource(selectedModel);
-            startCameraSource();
-        } else {
-            getRuntimePermissions();
-        }
+//        // An item was selected. You can retrieve the selected item using
+//        // parent.getItemAtPosition(pos)
+//        selectedModel = parent.getItemAtPosition(pos).toString();
+//        Log.d(TAG, "Selected model: " + selectedModel);
+//        preview.stop();
+//        if (allPermissionsGranted()) {
+//            createCameraSource(selectedModel);
+//            startCameraSource();
+//        } else {
+//            getRuntimePermissions();
+//        }
     }
 
     @Override
@@ -169,23 +206,23 @@ public final class LivePreviewActivity extends AppCompatActivity
     }
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.live_preview_menu, menu);
-        return true;
-    }
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        getMenuInflater().inflate(R.menu.live_preview_menu, menu);
+//        return true;
+//    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        if (item.getItemId() == R.id.settings) {
-            Intent intent = new Intent(this, SettingsActivity.class);
-            intent.putExtra(SettingsActivity.EXTRA_LAUNCH_SOURCE, LaunchSource.LIVE_PREVIEW);
-            startActivity(intent);
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        if (item.getItemId() == R.id.settings) {
+//            Intent intent = new Intent(this, SettingsActivity.class);
+//            intent.putExtra(SettingsActivity.EXTRA_LAUNCH_SOURCE, LaunchSource.LIVE_PREVIEW);
+//            startActivity(intent);
+//            return true;
+//        }
+//
+//        return super.onOptionsItemSelected(item);
+//    }
 
     private void createCameraSource(String model) {
         // If there's no existing cameraSource, create one.
@@ -195,42 +232,6 @@ public final class LivePreviewActivity extends AppCompatActivity
 
         try {
             switch (model) {
-                case CLASSIFICATION_QUANT:
-                    Log.i(TAG, "Using Custom Image Classifier (quant) Processor");
-                    cameraSource.setMachineLearningFrameProcessor(new CustomImageClassifierProcessor(this, true));
-                    break;
-                case CLASSIFICATION_FLOAT:
-                    Log.i(TAG, "Using Custom Image Classifier (float) Processor");
-                    cameraSource.setMachineLearningFrameProcessor(new CustomImageClassifierProcessor(this, false));
-                    break;
-                case TEXT_DETECTION:
-                    Log.i(TAG, "Using Text Detector Processor");
-                    cameraSource.setMachineLearningFrameProcessor(new TextRecognitionProcessor());
-                    break;
-                case FACE_DETECTION:
-                    Log.i(TAG, "Using Face Detector Processor");
-                    cameraSource.setMachineLearningFrameProcessor(new FaceDetectionProcessor(getResources()));
-                    break;
-                case AUTOML_IMAGE_LABELING:
-                    cameraSource.setMachineLearningFrameProcessor(new AutoMLImageLabelerProcessor(this, Mode.LIVE_PREVIEW));
-                    break;
-                case OBJECT_DETECTION:
-                    Log.i(TAG, "Using Object Detector Processor");
-                    FirebaseVisionObjectDetectorOptions objectDetectorOptions =
-                            new FirebaseVisionObjectDetectorOptions.Builder()
-                                    .setDetectorMode(FirebaseVisionObjectDetectorOptions.STREAM_MODE)
-                                    .enableClassification().build();
-                    cameraSource.setMachineLearningFrameProcessor(
-                            new ObjectDetectorProcessor(objectDetectorOptions));
-                    break;
-                case BARCODE_DETECTION:
-                    Log.i(TAG, "Using Barcode Detector Processor");
-                    cameraSource.setMachineLearningFrameProcessor(new BarcodeScanningProcessor());
-                    break;
-                case IMAGE_LABEL_DETECTION:
-                    Log.i(TAG, "Using Image Label Detector Processor");
-                    cameraSource.setMachineLearningFrameProcessor(new ImageLabelingProcessor());
-                    break;
                 case FACE_CONTOUR:
                     Log.i(TAG, "Using Face Contour Detector Processor");
                     cameraSource.setMachineLearningFrameProcessor(new FaceContourDetectorProcessor());
@@ -292,45 +293,6 @@ public final class LivePreviewActivity extends AppCompatActivity
         super.onDestroy();
         if (cameraSource != null) {
             cameraSource.release();
-        }
-    }
-
-    private String[] getRequiredPermissions() {
-        try {
-            PackageInfo info =
-                    this.getPackageManager()
-                            .getPackageInfo(this.getPackageName(), PackageManager.GET_PERMISSIONS);
-            String[] ps = info.requestedPermissions;
-            if (ps != null && ps.length > 0) {
-                return ps;
-            } else {
-                return new String[0];
-            }
-        } catch (Exception e) {
-            return new String[0];
-        }
-    }
-
-    private boolean allPermissionsGranted() {
-        for (String permission : getRequiredPermissions()) {
-            if (!isPermissionGranted(this, permission)) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    private void getRuntimePermissions() {
-        List<String> allNeededPermissions = new ArrayList<>();
-        for (String permission : getRequiredPermissions()) {
-            if (!isPermissionGranted(this, permission)) {
-                allNeededPermissions.add(permission);
-            }
-        }
-
-        if (!allNeededPermissions.isEmpty()) {
-            ActivityCompat.requestPermissions(
-                    this, allNeededPermissions.toArray(new String[0]), PERMISSION_REQUESTS);
         }
     }
 
