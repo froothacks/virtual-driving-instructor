df5f1cb04d7b2a95d28506f6bb5bb834515f2cba
Ethan Guo <ethguo@gmail.com>
2019-11-23 09:14:20 -0500

    Map doesn't work but hide api key

--- mlkit/app/src/main/java/com/google/firebase/samples/apps/mlkit/java/MapActivity.java
+++ mlkit/app/src/main/java/com/google/firebase/samples/apps/mlkit/java/MapActivity.java
@@ -3,7 +3,14 @@ package com.google.firebase.samples.apps.mlkit.java;
 import androidx.fragment.app.FragmentActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -13,9 +20,14 @@ import com.google.android.gms.maps.model.MarkerOptions;
 
 import com.google.firebase.samples.apps.mlkit.R;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class MapActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    RequestQueue queue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +37,9 @@ public class MapActivity extends FragmentActivity implements OnMapReadyCallback 
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        // Instantiate the RequestQueue.
+        queue = Volley.newRequestQueue(this);
     }
 
 
@@ -32,11 +47,38 @@ public class MapActivity extends FragmentActivity implements OnMapReadyCallback 
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        
+        String url = "https://maps.london.ca/arcgisa/rest/services/OpenData/OpenData_Transportation/MapServer/16/query?where=1%3D1&outFields=*&outSR=4326&f=json";
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject json = new JSONObject(response);
+                            JSONArray features = json.getJSONArray("features");
+                            for (int i = 0; i < features.length(); i++) {
+                                JSONObject f = features.getJSONObject(i);
+                                Boolean isSignaled = f.getJSONObject("attributes").getString("IntersectionType") == "Signalized Intersection";
+                                double x = f.getJSONObject("geometry").getDouble("x");
+                                double y = f.getJSONObject("geometry").getDouble("y");
+
+                                LatLng pos = new LatLng(x, y);
+                                mMap.addMarker(new MarkerOptions().position(pos));
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError e) {
+                e.printStackTrace();
+            }
+        });
+        queue.add(stringRequest);
 
         // Add a marker in Sydney and move the camera
         LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(43.0097095,-81.2748057), 15));
     }
 }
